8e87209fc6bc38fd9d5d63774c91108f9e5f2eea
Margherita <margherita.zanchini@studio.unibo.it>
2023-12-01 16:38:30 +0100

    completed 102

--- 102-advanced-mvc/src/main/java/it/unibo/mvc/DrawNumberApp.java
+++ 102-advanced-mvc/src/main/java/it/unibo/mvc/DrawNumberApp.java
@@ -1,15 +1,22 @@
 package it.unibo.mvc;
 
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 /**
  */
 public final class DrawNumberApp implements DrawNumberViewObserver {
-    private static final int MIN = 0;
-    private static final int MAX = 100;
-    private static final int ATTEMPTS = 10;
+    //private static final int MIN = 0;
+    //private static final int MAX = 100;
+    //private static final int ATTEMPTS = 10;
+
+    private static final String FILE_NAME = "config.yml";
 
     private final DrawNumber model;
     private final List<DrawNumberView> views;
@@ -22,12 +29,25 @@ public final class DrawNumberApp implements DrawNumberViewObserver {
         /*
          * Side-effect proof
          */
+        List<Integer> param = new ArrayList<>();
+        try {
+            param = getParam();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
         this.views = Arrays.asList(Arrays.copyOf(views, views.length));
         for (final DrawNumberView view: views) {
             view.setObserver(this);
             view.start();
         }
-        this.model = new DrawNumberImpl(MIN, MAX, ATTEMPTS);
+
+        final Configuration.Builder configurationBuilder = new Configuration.Builder();
+        configurationBuilder.setMin(param.get(0));
+        configurationBuilder.setMax(param.get(1));
+        configurationBuilder.setAttempts(param.get(2));
+        Configuration config = configurationBuilder.build();
+        this.model = new DrawNumberImpl(config.getMin(), config.getMax(), config.getAttempts());
     }
 
     @Override
@@ -59,14 +79,36 @@ public final class DrawNumberApp implements DrawNumberViewObserver {
          */
         System.exit(0);
     }
+    
+    public List<Integer> getParam() throws IOException{
+        List<Integer> res = new ArrayList<>();
+        try {
+            final InputStream is = getClass().getClassLoader().getResourceAsStream(FILE_NAME);
+            final BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            String line;
+            while((line = br.readLine()) != null){
+                String[] splitLine = line.split(": ", 2);
+                res.add(Integer.parseInt(splitLine[1]));
+            }
+
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        
+        return res;
+    }
 
     /**
      * @param args
      *            ignored
-     * @throws FileNotFoundException 
+     * @throws IOException
      */
-    public static void main(final String... args) throws FileNotFoundException {
-        new DrawNumberApp(new DrawNumberViewImpl());
+    public static void main(final String... args) throws IOException {
+        new DrawNumberApp(new DrawNumberViewImpl(),
+                            new DrawNumberViewImpl(),
+                            new PrintStreamView(System.out),
+                            new PrintStreamView("Output.log"));
     }
 
 }
